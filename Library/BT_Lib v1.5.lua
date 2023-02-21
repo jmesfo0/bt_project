ff972c4de417d4a7446b9e7367b497d2dbf664ce
jmes <james@jmes.dev>
2023-02-20 18:17:04 -0800

    Update BT_Lib v1.5.lua

--- Library/BT_Lib v1.5.lua
+++ Library/BT_Lib v1.5.lua
@@ -3124,7 +3124,7 @@ function Library:CreateWindow(title, gameName)
                     label.BorderSizePixel = 0
                     label.ClipsDescendants = true
                     label.Text = title
-                    label.Size = UDim2.new(1, 0, 0, string.len(label.Text))
+                    label.Size = UDim2.new(1, 0, 0, 30)
                     label.Font = Enum.Font.SourceSansBold
                     label.Text = "  "..title
                     label.RichText = true
@@ -3141,7 +3141,8 @@ function Library:CreateWindow(title, gameName)
                     end 
                     if Theme.AccentColor == Color3.fromRGB(0,0,0) then
                         Utility:TweenObject(label, {TextColor3 = Color3.fromRGB(255,255,255)}, 0.2)
-                    end
+                    end 
+    
                     coroutine.wrap(function()
                         while wait() do
                             label.BackgroundColor3 = Theme.AccentColor
