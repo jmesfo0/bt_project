0b2947a02182d6f9f93a5c20efe7a81c7213a765
James <42681518+jmesfo0@users.noreply.github.com>
2023-03-27 09:17:28 -0700

    Update BT_Lib v1.5.lua

--- Library/BT_Lib v1.5.lua
+++ Library/BT_Lib v1.5.lua
@@ -3065,8 +3065,8 @@ function Library:CreateWindow(title, gameName)
                     TextLabel.TextYAlignment = Enum.TextYAlignment.Top
 					
 					TextLabel:GetPropertyChangedSignal("Text"):Connect(function()
-						Frame.CanvasSize = UDim2.new(0, 0, 0, string.len(TextLabel.Text) - 50)
-						TextLabel.Size = UDim2.new(1, -10, 0, string.len(TextLabel.Text) - 50)
+						Frame.CanvasSize = UDim2.new(0, 0, 0, string.len(TextLabel.Text))
+						TextLabel.Size = UDim2.new(1, -10, 0, string.len(TextLabel.Text))
 					end)
                     
                     coroutine.wrap(function()
